044e09145f3651f00fb69304cb4ef77cc796a9c6
TonyKmoch <tony.kmoch@gmail.com>
2025-12-22 18:20:57 +0100

    showing value nicely in the icon

--- TrayApp/Program.cs
+++ TrayApp/Program.cs
@@ -1,35 +1,68 @@
 using System;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Windows.Forms;
 
 namespace TrayApp
 {
     static class Program
     {
+        private static NotifyIcon trayIcon;
+        private static int glucoseValue = 105; // SAMPLE VALUE
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Create a NotifyIcon (System Tray icon)
-            NotifyIcon trayIcon = new NotifyIcon();
-            trayIcon.Icon = SystemIcons.Application; // default app icon
-            trayIcon.Text = "My Tray App";
+            trayIcon = new NotifyIcon();
             trayIcon.Visible = true;
+            trayIcon.Text = "Glucose Monitor";
 
-            // Add a context menu
+            trayIcon.Icon = CreateGlucoseIcon(glucoseValue);
+
+            // Context menu
             ContextMenuStrip menu = new ContextMenuStrip();
-            ToolStripMenuItem exitItem = new ToolStripMenuItem("Exit");
-            exitItem.Click += (s, e) => Application.Exit();
-            menu.Items.Add(exitItem);
+            menu.Items.Add("Exit", null, (s, e) => Application.Exit());
             trayIcon.ContextMenuStrip = menu;
 
-            // Optional: show balloon tip on start
-            trayIcon.ShowBalloonTip(3000, "Tray App", "Application started!", ToolTipIcon.Info);
-
-            // Run invisible application
             Application.Run();
+        }
+
+        static Icon CreateGlucoseIcon(int value)
+        {
+            Color bgColor = GetGlucoseColor(value);
+
+            Bitmap bmp = new Bitmap(16, 16);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(bgColor);
+                g.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
+
+                using (Font font = new Font("Segoe UI", 8, FontStyle.Bold, GraphicsUnit.Pixel))
+                using (Brush textBrush = Brushes.White)
+                {
+                    string text = value.ToString();
+                    SizeF size = g.MeasureString(text, font);
+
+                    float x = (16 - size.Width) / 2;
+                    float y = (16 - size.Height) / 2 - 1;
+
+                    g.DrawString(text, font, textBrush, x, y);
+                }
+            }
+
+            return Icon.FromHandle(bmp.GetHicon());
+        }
+
+        static Color GetGlucoseColor(int value)
+        {
+            if (value < 70)
+                return Color.Red;        // Low
+            if (value <= 140)
+                return Color.Green;      // Normal
+            return Color.Orange;         // High
         }
     }
 }
